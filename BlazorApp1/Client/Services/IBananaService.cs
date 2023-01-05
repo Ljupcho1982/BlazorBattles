76f535f3932873d355b6c4622119ff1fb6bc0807
Ljupcho Shemov <ljupco.semov@gmail.com>
2023-01-05 11:52:39 +0100

    Add/Eat Bananas

---
+++ BlazorApp1/Client/Services/IBananaService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp1.Client.Services
+{
+    public interface IBananaService
+    {
+        public event Action OnChange;
+        int Bananas { get; set; }
+
+        void EatBananas(int amount);
+
+        void AddBananas(int amount);
+    }
+}
